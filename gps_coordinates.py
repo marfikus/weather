c75508cf0722b5dedb18232b28c8ad6667272c18
Alexey Zaytsev <alex.rv9rh@gmail.com>
2022-06-01 14:03:06 +0500

    Refactor gps_coordinates.py

--- gps_coordinates.py
+++ gps_coordinates.py
@@ -1,7 +1,9 @@
 
 from dataclasses import dataclass
-from exceptions import CantGetCoordinates
+from typing import Literal
 import os
+
+from exceptions import CantGetCoordinates
 import config
 
 
@@ -14,29 +16,60 @@ class Coordinates:
 def get_gps_coordinates() -> Coordinates:
     """Return current gps coordinates"""
 
-    filename = "coordinates.txt"
+    coordinates = _get_coordinates_from_file()
+    return _round_coordinates(coordinates)
 
+
+def _get_coordinates_from_file() -> Coordinates:
+    filename = "coordinates.txt"
+    lines = _get_strings_from_file(filename)
+
+    return Coordinates(
+        latitude=_parse_coordinate(lines, "latitude"),
+        longitude=_parse_coordinate(lines, "longitude")
+    )
+
+
+def _get_strings_from_file(filename: str) -> list[str]:
     if not (os.path.exists(filename)):
         raise CantGetCoordinates("file not exists")
 
     with open(filename, encoding="utf-8") as f:
-        lines = f.readlines()
+        try:
+            lines = f.readlines()
+        except UnicodeDecodeError:
+            raise CantGetCoordinates("file decode error")
     
     if len(lines) == 0:
         raise CantGetCoordinates("file is empty")
 
-    latitude = longitude = None
+    return lines
 
+
+def _parse_coordinate(
+    lines: list[str],
+    coord_type: Literal["latitude", "longitude"]) -> float:
+
+    coordinate = None
     for line in lines:
         line = line.strip().lower()
-        if line.startswith("latitude"):
-            latitude = float(line.split()[1])
-        if line.startswith("longitude"):
-            longitude = float(line.split()[1])
+        if line.startswith(f"{coord_type}:"):
+            try:
+                coordinate = float(line.split()[1])
+            except ValueError:
+                raise CantGetCoordinates("parsing to float error")
+    
+    if coordinate is None:
+        raise CantGetCoordinates("coordinate not found")
+    else:
+        return coordinate
 
-    if config.USE_ROUNDED_COORDINATES:
-        latitude, longitude = map(lambda c: round(c, 1), [latitude, longitude])
 
-    return Coordinates(latitude=latitude, longitude=longitude)
+def _round_coordinates(coordinates: Coordinates) -> Coordinates:
+    if not config.USE_ROUNDED_COORDINATES:
+        return coordinates
 
-# print(get_gps_coordinates())
+    return Coordinates(*map(
+        lambda c: round(c, 1),
+        [coordinates.latitude, coordinates.longitude]
+    ))
