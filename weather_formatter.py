2140c8772f7015765ae604b4dba6f4da960388da
Alexey Zaytsev <alex.rv9rh@gmail.com>
2022-06-01 17:14:37 +0500

    Update weather_formatter

--- weather_formatter.py
+++ weather_formatter.py
@@ -3,5 +3,21 @@ from weather_api_service import Weather
 
 
 def format_weather(weather: Weather) -> str:
-    """Format weather data to string"""
-    return f"{weather.city} {weather.temperature} {weather.weather_type}"
+    """Format weather data in string"""
+    return (f"{weather.city}, температура {weather.temperature}°C, "
+            f"{weather.weather_type}\n"
+            f"Восход: {weather.sunrise.strftime('%H:%M')}\n"
+            f"Закат: {weather.sunset.strftime('%H:%M')}\n")
+
+
+if __name__ == "__main__":
+    from datetime import datetime
+    from weather_api_service import WeatherType
+
+    print(format_weather(Weather(
+        temperature=20,
+        weather_type=WeatherType.CLEAR,
+        sunrise=datetime.fromisoformat("2022-05-30 04:30:00"),
+        sunset=datetime.fromisoformat("2022-05-30 22:30:00"),
+        city="London"
+    )))
